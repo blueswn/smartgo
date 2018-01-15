205798049cdade63818a9ba60b1575a42da18597
leo.qin <Leo.Qin@firstleap.cn>
2018-01-15 16:22:15 +0800

    smartgo

--- main.go
+++ main.go
@@ -3,12 +3,19 @@ package main
 import (
 	"net/http"
 	"fmt"
+	"log"
 )
 
 func main()  {
 	http.HandleFunc("/router/rest", ServeHTTP)
 	http.ListenAndServe(":8002", nil)
 }
+
+const (
+	// Version is Framework's version.
+	Version                = "v1.0"
+	defaultMultipartMemory = 32 << 20 // 32 MB
+)
 
 //Request represents an HTTP request.
 type Request struct {
@@ -19,26 +26,26 @@ type Request struct {
 }
 
 //Create a new Illuminate HTTP request from server variables.
-func (Request) capture(r *http.Request) (Request) {
+func (*Request) capture(r *http.Request) {
 	method := r.Method
 	query := r.URL.Query()
 
 	action, ok := query["action"];
 	if !ok {
 		//fmt.Fprintln(w, "404");
-		panic(404)
+		panic("21 missing action")
 	}
 	delete(query, "action")
 	req := Request{}
 	req.request = ParameterBag{parameters:query}
 	req.action = action[(len(action) -1)]
 	req.method = method
-	return req;
+	return;
 }
 
 //Response represents an HTTP response.
 type Response struct {
-
+	context string
 }
 
 //Sets the response content.
@@ -69,20 +76,37 @@ type Application struct {
 }
 
 func (app Application) version() (string) {
-	return "1.0.0"
+	return Version
 }
 
 //Handle an incoming HTTP request.
-func (app Application) handle(req Request) (Response) {
+func (app *Application) handle(req Request) (Response) {
+	res := Response{};
+	res.context = "1234"
+	app.response = res;
 	return app.response
 }
 
 func ServeHTTP(w http.ResponseWriter, r *http.Request)  {
+	defer func() {
+		log.Println("done")
+		// Println executes normally even if there is a panic
+		if err := recover(); err != nil {
+			log.Printf("run time panic: %v", err)
+			fmt.Fprintln(w, err)
+		}
+	}()
+	log.Println("start")
+
 	req := Request{}
-	req = req.capture(r)
+	req.capture(r)
 
 	app := Application{}
+	fmt.Fprintln(w, app)
 	response := app.handle(req)
+	fmt.Fprintln(w, app)
+	newReq := req
+	fmt.Println(newReq);
 
 	//fmt.Fprintln(w, req)
 	//fmt.Println(req)
