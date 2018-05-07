9a550a667a780ac4c6333c5869e0be730abbda6d
leo.qin <Leo.Qin@firstleap.cn>
2018-05-07 11:17:15 +0800

    smartgo

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ type Request struct {
 }
 
 //Create a new Illuminate HTTP request from server variables.
-func (*Request) capture(r *http.Request) {
+func (req Request) capture(r *http.Request) Request{
 	method := r.Method
 	query := r.URL.Query()
 
@@ -36,11 +36,11 @@ func (*Request) capture(r *http.Request) {
 		panic("21 missing action")
 	}
 	delete(query, "action")
-	req := Request{}
+	//req := Request{}
 	req.request = ParameterBag{parameters:query}
 	req.action = action[(len(action) -1)]
 	req.method = method
-	return;
+	return req;
 }
 
 //Response represents an HTTP response.
@@ -59,6 +59,7 @@ func (res Response) setStatusCode()  {
 func (res Response) setProtocolVersion()  {
 
 }
+
 //Sends HTTP headers and content.
 func (res Response) send(w http.ResponseWriter) {
 	fmt.Fprintln(w, "hello world")
@@ -88,25 +89,31 @@ func (app *Application) handle(req Request) (Response) {
 }
 
 func ServeHTTP(w http.ResponseWriter, r *http.Request)  {
+	w.Header().Add("Access-Control-Allow-Origin", "*")
+	//w.Header().Add("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json;charset=utf-8")
 	defer func() {
 		log.Println("done")
 		// Println executes normally even if there is a panic
 		if err := recover(); err != nil {
 			log.Printf("run time panic: %v", err)
 			fmt.Fprintln(w, err)
+			//http.Error(w, "11111", 22)
 		}
 	}()
 	log.Println("start")
 
 	req := Request{}
-	req.capture(r)
+	req = req.capture(r)
 
-	app := Application{}
-	fmt.Fprintln(w, app)
-	response := app.handle(req)
-	fmt.Fprintln(w, app)
+	//app := Application{}
+	//fmt.Fprintln(w, app)
+	//response := app.handle(req)
+	//fmt.Fprintln(w, app)
 	newReq := req
-	fmt.Println(newReq);
+	fmt.Println("%#v\n", newReq)
+	jsonStr := `{"a":"b", "b":"c"}`
+	fmt.Fprintln(w, jsonStr)
 
 	//fmt.Fprintln(w, req)
 	//fmt.Println(req)
@@ -114,7 +121,7 @@ func ServeHTTP(w http.ResponseWriter, r *http.Request)  {
 	//fmt.Println(req.action)
 	//fmt.Println(response)
 
-	response.send(w)
+	//response.send(w)
 	//fmt.Fprintln(w, "hello world")
 	//fmt.Fprintln(w, action)
 }
